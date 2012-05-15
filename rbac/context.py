2f493d532c593018e325fd5dcaab08d719573e49
tonyseek <tonyseek@gmail.com>
2012-05-16 01:21:34 +0800

    Added some document into the context module.

--- rbac/context.py
+++ rbac/context.py
@@ -25,9 +25,24 @@ class IdentityContext(object):
         self.set_roles_loader(roles_loader)
 
     def set_roles_loader(self, role_loader):
+        """Set a callable object (such as a function) which could return a
+        iteration to provide all roles of current context user.
+
+        Example:
+        >>> @context.set_roles_loader
+        ... def load_roles():
+        ...     user = request.context.current_user
+        ...     for role in user.roles:
+        ...         yield role
+        """
         self.load_roles = role_loader
 
     def check_permission(self, operation, resource, **exception_kwargs):
+        """A decorator to check the permission.
+
+        The keyword arguments would be stored into the attribute `kwargs` of
+        the exception `PermissionDenied`.
+        """
         checker = functools.partial(self._docheck, operation=operation,
                                     resource=resource, **exception_kwargs)
         return functools.partial(PermissionChecker, checker)
