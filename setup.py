25b35a1d0ccae0cb7929ce16019d1da9ed5d71b7
tonyseek <tonyseek@gmail.com>
2012-06-05 05:09:52 +0800

    Prepared to upload on PyPI.

--- setup.py
+++ setup.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-import setuptools
+from setuptools import setup
 
 
-metadata = {'name': "Simple RBAC",
+long_description = open("./README.rst", "r").read().decode("utf-8")
+
+classifiers = [
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Topic :: Security",
+            "Topic :: Software Development :: Libraries :: Python Modules"]
+
+metadata = {'name': "simple-rbac",
             'version': "0.1",
             'description': "A simple role based access control utility",
+            'long_description': long_description,
             'keywords': "rbac permission acl access-control",
             'author': "TonySeek",
             'author_email': "tonyseek@gmail.com",
-            'url': "http://tonyseek.github.com/simple-rbac/",
+            'url': "http://github.tonyseek.com/simple-rbac/",
             'license': "MIT License",
             'packages': ["rbac"],
             'zip_safe': True,
             'platforms': "any",
-            'test_suite': "tests.run_tests"}
+            'test_suite': "tests.run_tests",
+            'classifiers': classifiers}
 
 
 if __name__ == "__main__":
-    setuptools.setup(**metadata)
+    setup(**metadata)
